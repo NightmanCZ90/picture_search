f70f51abcb23e0cae7a20960416faa4d01fe959f
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-12 18:25:12 +0100

    added a function to fetch api data images from unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term)
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos/', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID K9Y1ubd8CRzV_ousW3oSRA6qFiLh36T4AzZelkDnKGQ'
+      }  
+    });
+
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar 
-          onSubmit={this.onSearchSubmit}
-        />
+        <SearchBar onSubmit={ this.onSearchSubmit } />
+        Found: {this.state.images.length} images
       </div>
     )  
   }
